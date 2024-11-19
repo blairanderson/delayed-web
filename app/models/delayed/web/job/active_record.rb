879877c6f389ecfddd4cd289e0fee59715ae12b0
Blair Anderson <blair81@gmail.com>
2024-11-19 09:56:51 -0800

    order by failed first

--- app/models/delayed/web/job/active_record.rb
+++ app/models/delayed/web/job/active_record.rb
@@ -1,12 +1,12 @@
 module Delayed
   module Web
     class Job::ActiveRecord
-      def self.find *args
+      def self.find(*args)
         decorate Delayed::Job.find(*args)
       end
 
       def self.all
-        jobs = Delayed::Job.order('id DESC').limit(1000)
+        jobs = Delayed::Job.order(Arel.sql("CASE WHEN failed_at IS NOT NULL THEN 0 ELSE 1 END, failed_at ASC NULLS LAST, id DESC")).limit(1000)
         Enumerator.new do |enumerator|
           jobs.each do |job|
             enumerator.yield decorate(job)
@@ -18,13 +18,13 @@ module Delayed
         Delayed::Job.group(handler_to_job_name).count.sort_by { |_, v| -v }.to_h
       end
 
-      def self.decorate job
+      def self.decorate(job)
         job = StatusDecorator.new job
         job = ActiveRecordDecorator.new job
         job
       end
 
-      def self.by_job_name(job_name=nil)
+      def self.by_job_name(job_name = nil)
         scope = Delayed::Job.where(locked_at: nil, locked_by: nil).where("last_error IS NOT NULL OR failed_at IS NOT NULL").select("*, #{handler_to_job_name} as job_name")
         if job_name
           scope = scope.where("#{handler_to_job_name} = ?", job_name)
@@ -39,12 +39,12 @@ module Delayed
 
       def self.handler_to_job_name
         case Delayed::Job.connection.adapter_name
-          when "PostgreSQL"
-            "SUBSTRING(delayed_jobs.handler, ':(\\w+){1}\\n')"
-          when "MySQL", "Mysql2"
-            "SUBSTRING(delayed_jobs.handler, ':(\\w+){1}\\n')"
-          else
-            raise("Missing Handler function for #{Delayed::Job.connection.adapter_name}")
+        when "PostgreSQL"
+          "SUBSTRING(delayed_jobs.handler, ':(\\w+){1}\\n')"
+        when "MySQL", "Mysql2"
+          "SUBSTRING(delayed_jobs.handler, ':(\\w+){1}\\n')"
+        else
+          raise("Missing Handler function for #{Delayed::Job.connection.adapter_name}")
         end
       end
     end
